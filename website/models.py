4f4e923bbcca812201a9956c544725f7c00a392d
umesh <kumar886umesh@gmail.com>
2016-12-13 23:32:32 +0530

    model edit

--- website/models.py
+++ website/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=50)
+    fname = models.CharField(max_length=50)
     email = models.EmailField(max_length=250)
     password = models.CharField(max_length=50)
 
