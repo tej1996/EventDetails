13f04297674a9723fd8517fd617d5d391b4b2a9b
tej1996 <tej1996@gmail.com>
2016-12-15 23:22:31 +0530

    Registered Events Model in Admin

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from website.models import UserProfile
+from website.models import UserProfile,Events
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(Events)
