a1255da468f4faca4aefb229260c2f9e5529f835
tej1996 <tej1996@gmail.com>
2016-12-15 23:23:20 +0530

    Added User and UserProfile Model Combined

--- website/models.py
+++ website/models.py
@@ -1,13 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
 
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=250)
-    password = models.CharField(max_length=50)
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User, null=True, blank=True)
 
-    def __str__(self):
-        return 'Name: ' + self.name
+    # The additional attributes we wish to include.
+    name = models.CharField(max_length=50, default=None)
+    age = models.IntegerField(default=None, blank=True, null=True)
 
+    # Override the __unicode__() method to return out something meaningful!
+    def __unicode__(self):
+        return self.user.username
+
+
+class Events(models.Model):
+    event_name = models.CharField(max_length=20)
+    description = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    by = models.IntegerField(default=23)
